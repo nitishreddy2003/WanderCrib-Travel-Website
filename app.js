d2aaec0660146b1ebf9ebbc0e10f06cd3be7ab40
nitishreddy2003 <nitish.200327@gmail.com>
2025-05-31 23:46:46 +0530

    Fixed a error in middleware

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((res,req,next)=>{
+app.use((req,res,next)=>{
   res.locals.success = req.flash("success");
   res.locals.error= req.flash("error");
   res.locals.currUser=req.user;
