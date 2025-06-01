91fd3f138173710e440e40923c187d5ad01f55a6
nitishreddy2003 <nitish.200327@gmail.com>
2025-06-02 03:06:55 +0530

    Created signup-login direct route and given user specific functionalities to only logged in users

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const flash=require('connect-flash');
 const passport=require('passport');
 const LocalStrategy=require('passport-local');
 const User=require('./models/user.js');
-
+const wrapasync=require('./utils/wrapAsync.js')
 
 const listingRoute=require("./routes/listing.js");
 const reviewRoute=require('./routes/review.js');
